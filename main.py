c970a22b81b0e0d8c853c1be225030ca9e27441b
israelwangila <wangilaisrael@gmail.com>
2020-09-06 22:11:43 +0300

    importing datamanip in the main.py

--- main.py
+++ main.py
@@ -7,3 +7,7 @@ from os.path import isfile
 from hashlib import sha256
 from termcolor import colored
 from halo import Halo
+
+from modules.encryption import DataManip
+from modules.exceptions import UserExits, PasswordFileDoesNotExist
+from modules.menu import Manager
